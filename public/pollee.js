5aa297fecb4676114f8d107057a59f75e0e6c683
Dan Winter <dan.j.winter@gmail.com>
2016-04-24 13:14:01 -0600

    add partials

--- public/pollee.js
+++ public/pollee.js
@@ -1,31 +1,28 @@
 var socket = io();
 
-var results = document.querySelector('.results');
-
 var buttons = document.querySelectorAll('#choices button');
 var buttonArea = document.querySelector('#choices');
 var currentId = window.location.href.split("survey/")[1];
+var yourVote = document.querySelector('#your-vote');
+var choices = "";
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
     socket.send('voteCast', {id: currentId, vote: this.innerText, socketId: socket.id});
+    yourVote.innerText = `Your vote: ${this.innerText}`;
   });
 }
 
+for (var i = 0; i < buttons.length; i++) {
+  choices = choices + `${i + 1}:` + buttons[i].innerText;
+}
+
 function closeThisPoll() {
-  buttonArea.innerText = "Sorry, this survey has been closed";
+  buttonArea.innerText = `Sorry, this survey has been closed. The choices were\n${choices}`;
 }
 
 socket.on('pollClosed', function (id) {
   if (currentId == id) {
     closeThisPoll();
   }
-});
-
-function showResults(message) {
-  results.innerText = message.resultText;
-}
-
-socket.on('results', function (message) {
-  showResults(message);
 });
